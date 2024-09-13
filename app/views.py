975712a8ea3122047c098cdb0f12ccbc8801e2b6
Roman Saklakov <Roman-S.89@mail.ru>
2024-09-13 09:53:55 +0300

    Initial commit

---
+++ app/views.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+import datetime
+import os
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.datetime.now()
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    list_files = os.listdir(path = '.')
+    msg = f'Файлы в текущей директории: {list_files}'
+    return HttpResponse(msg)
+
